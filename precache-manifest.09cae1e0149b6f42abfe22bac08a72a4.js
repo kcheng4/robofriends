c9125026b84711fb7ceb3fabd2d4912bef684267
Kenny <kcheng4@villanova.edu>
2019-01-29 19:07:52 -0500

    Updates

---
+++ precache-manifest.09cae1e0149b6f42abfe22bac08a72a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f48a775ce8e70abc1382771a3bfaa26",
+    "url": "/robofriends/static/media/NOVABOLD.9f48a775.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "28510872cfa3b7ac5446",
+    "url": "/robofriends/static/js/main.28510872.chunk.js"
+  },
+  {
+    "revision": "cf2bc93423c995590bc0",
+    "url": "/robofriends/static/js/1.cf2bc934.chunk.js"
+  },
+  {
+    "revision": "28510872cfa3b7ac5446",
+    "url": "/robofriends/static/css/main.ab53502c.chunk.css"
+  },
+  {
+    "revision": "cf2bc93423c995590bc0",
+    "url": "/robofriends/static/css/1.9a386687.chunk.css"
+  },
+  {
+    "revision": "7e7941e93717d3ee7f00096feb3372e5",
+    "url": "/robofriends/index.html"
+  }
+];
